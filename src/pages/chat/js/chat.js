f8cc106fc61bccbbecfbb5f887ee020ded04508c
Kaue-Alves <akaue325@gmail.com>
2025-01-23 21:21:02 -0300

    Adiciona configuração do servidor WebSocket e estrutura inicial do chat

--- src/pages/chat/js/chat.js
+++ src/pages/chat/js/chat.js
@@ -1,1 +1,148 @@
-alert("Em desenvolvimento");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import {
+    getAuth,
+    onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAB0t3hUxpZtCPTEmmbVEfHGWqfd4xqgNU",
+    authDomain: "correio-do-amor-48a9f.firebaseapp.com",
+    projectId: "correio-do-amor-48a9f",
+    storageBucket: "correio-do-amor-48a9f.firebasestorage.app",
+    messagingSenderId: "400456218648",
+    appId: "1:400456218648:web:edec8e20ba12436a792b6d",
+    measurementId: "G-62RY0QRZCG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+let firstName;
+
+function updateUserProfile(user) {
+    let userName = user.displayName;
+    let userString = userName.split(" ");
+    firstName = userString[0];
+    const userProfilePicture = user.photoURL;
+
+    document.getElementById("userName").textContent = `Olá ${firstName}`;
+    document.getElementById("userProfilePicture").src = userProfilePicture;
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        updateUserProfile(user);
+    } else {
+        localStorage.removeItem("userProfileData")
+        alert("Create Account & Login");
+    }
+});
+
+
+// login elements
+const login = document.querySelector(".login")
+const loginForm = login.querySelector(".login__form")
+const loginInput = login.querySelector(".login__input")
+
+// chat elements
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat__form")
+const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages")
+
+const colors = [
+    "cadetblue",
+    "darkgoldenrod",
+    "cornflowerblue",
+    "darkkhaki",
+    "hotpink",
+    "gold"
+]
+
+const user = { id: "", name: "", color: "" }
+
+let websocket
+
+const createMessageSelfElement = (content) => {
+    const div = document.createElement("div")
+
+    div.classList.add("message--self")
+    div.innerHTML = content
+
+    return div
+}
+
+const createMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement("div")
+    const span = document.createElement("span")
+
+    div.classList.add("message--other")
+
+    span.classList.add("message--sender")
+    span.style.color = senderColor
+
+    div.appendChild(span)
+
+    span.innerHTML = sender
+    div.innerHTML += content
+
+    return div
+}
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
+
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
+const processMessage = ({ data }) => {
+    const { userId, userName, userColor, content } = JSON.parse(data)
+
+    const message =
+        userId == user.id
+            ? createMessageSelfElement(content)
+            : createMessageOtherElement(content, userName, userColor)
+
+    chatMessages.appendChild(message)
+
+    scrollScreen()
+}
+
+const handleLogin = (event) => {
+    event.preventDefault()
+
+    user.id = crypto.randomUUID()
+    user.name = firstName
+    user.color = getRandomColor()
+
+    login.style.display = "none"
+    chat.style.display = "flex"
+
+    websocket = new WebSocket("ws://localhost:8080")
+    websocket.onmessage = processMessage
+}
+
+const sendMessage = (event) => {
+    event.preventDefault()
+
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    }
+
+    websocket.send(JSON.stringify(message))
+
+    chatInput.value = ""
+}
+
+loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit", sendMessage)
